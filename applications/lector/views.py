97b329d7c3764fd1897f8e75816d08549be71016
ThomasC-Program <thomascriollocevallos@gmail.com>
2026-07-20 17:25:25 -0500

    Modulo 13 VID 144 Formulario Prestamo

--- applications/lector/views.py
+++ applications/lector/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
+from .models import Prestamo
 
+from .forms import PrestamoForm
 # Create your views here.
+
+class RegistrarPrestamo(FormView):
+    template_name = 'prestamo/add.html'
+    form_class = PrestamoForm
+    success_url = '.'
+    
+    def form_valid(self, form):
+        
+        return super(RegistrarPrestamo, self).form_valid(form)
